297ddcf33d4f247e6401b3fdc4b8c70584ec2798
Pablo Schwarzenberg <pablo.schwarzenberg@unab.cl>
2024-11-06 21:57:04 -0300

    0611

--- sesion6/clase2808/mbert_mask.py
+++ sesion6/clase2808/mbert_mask.py
@@ -1,5 +1,3 @@
 from transformers import pipeline
 unmasker = pipeline('fill-mask', model='google/mobilebert-uncased')
 print(unmasker("The cat is [MASK]."))
-
-print(unmasker("The dog is [MASK]."))
